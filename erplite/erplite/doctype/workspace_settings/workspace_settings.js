b15b4fc12175f1321ac96c658d020ff8ef72beed
SherinKR <sherinkrply@gmail.com>
2023-01-11 11:41:56 +0530

    feat: Workspace for Changemakers

---
+++ erplite/erplite/doctype/workspace_settings/workspace_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, T4G Labs and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Workspace Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ erplite/erplite/doctype/workspace_settings/workspace_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, T4G Labs and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class WorkspaceSettings(Document):
+	pass

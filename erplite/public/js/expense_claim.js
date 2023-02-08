db62de25fab9299832b0ee257935a5b38b282486
SherinKR <sherinkrply@gmail.com>
2023-02-08 12:59:34 +0530

    feat: Fetching Expenses from Travel Request to Expense Claim

--- erplite/public/js/expense_claim.js
+++ erplite/public/js/expense_claim.js
@@ -1,9 +1,11 @@
 frappe.ui.form.on("Expense Claim", {
   setup: function(frm){
+    set_filters(frm);
     frm.set_df_property('approval_status', 'read_only', 1);
     frm.refresh_field('approval_status');
   },
   refresh: function(frm) {
+    set_filters(frm);
     frm.set_df_property('approval_status', 'read_only', 1);
     frm.refresh_field('approval_status');
     if (!frm.is_new()){
@@ -16,5 +18,41 @@ frappe.ui.form.on("Expense Claim", {
     else {
       frm.set_value('approval_status', 'Draft');
     }
+  },
+  travel_request: function(frm){
+    if(frm.doc.travel_request){
+      set_expenses_from_travel_request(frm, frm.doc.travel_request);
+    }
   }
-})
+});
+
+function set_filters(frm) {
+  frm.set_query("travel_request", function() {
+    return {
+      "filters": {
+        "docstatus": 1,
+        "employee": frm.doc.employee
+      }
+    };
+  });
+}
+
+function set_expenses_from_travel_request(frm, travel_request){
+  if(travel_request){
+    frappe.db.get_doc('Travel Request', travel_request).then(doc => {
+      if(doc.costings){
+        frm.clear_table('expenses');
+        frm.refresh_field('expenses');
+        doc.costings.forEach(costing => {
+          let expense = frm.add_child('expenses');
+          expense.expense_date = frm.doc.posting_date;
+          expense.expense_type = costing.expense_type;
+          expense.description = costing.comments;
+          expense.amount = costing.total_amount;
+          expense.sanctioned_amount = costing.total_amount;
+        });
+        frm.refresh_field('expenses');
+      }
+    });
+  }
+}
